7eb755edbd6cb1d7f2abf6c1be8cdfef419c3515
amesgen <amesgen@amesgen.de>
2021-06-24 14:01:05 +0200

    ghc-lib-parser-8.10.5.20210606

---
+++ ghc-lib-parser-8.10.5.20210606/ghc-lib/stage0/libraries/ghc-boot/build/GHC/Version.hs
@@ -0,0 +1,21 @@
+module GHC.Version where
+
+import Prelude -- See Note [Why do we import Prelude here?]
+
+cProjectGitCommitId   :: String
+cProjectGitCommitId   = "a43a5650f51fdc04d757abc3d86b0a23ec518259"
+
+cProjectVersion       :: String
+cProjectVersion       = "8.10.5"
+
+cProjectVersionInt    :: String
+cProjectVersionInt    = "810"
+
+cProjectPatchLevel    :: String
+cProjectPatchLevel    = "5"
+
+cProjectPatchLevel1   :: String
+cProjectPatchLevel1   = "5"
+
+cProjectPatchLevel2   :: String
+cProjectPatchLevel2   = ""
