5d173a747118880ded6568001c8d0d56b57ac69f
amesgen <amesgen@amesgen.de>
2021-08-04 00:20:26 +0200

    add ghc-lib-parser-9.0.1.20210324

---
+++ ghc-lib-parser-9.0.1.20210324/ghc-lib/stage0/compiler/build/GHC/Settings/Config.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE CPP #-}
+module GHC.Settings.Config
+  ( module GHC.Version
+  , cBuildPlatformString
+  , cHostPlatformString
+  , cProjectName
+  , cBooterVersion
+  , cStage
+  ) where
+
+import GHC.Prelude
+
+import GHC.Version
+
+cBuildPlatformString :: String
+cBuildPlatformString = "x86_64-apple-darwin"
+
+cHostPlatformString :: String
+cHostPlatformString = "x86_64-apple-darwin"
+
+cProjectName          :: String
+cProjectName          = "The Glorious Glasgow Haskell Compilation System"
+
+cBooterVersion        :: String
+cBooterVersion        = "8.8.3"
+
+cStage                :: String
+cStage                = show (1 :: Int)
---
+++ ghc-lib-parser-9.0.1.20210324/ghc-lib/stage0/libraries/ghc-boot/build/GHC/Version.hs
@@ -0,0 +1,21 @@
+module GHC.Version where
+
+import Prelude -- See Note [Why do we import Prelude here?]
+
+cProjectGitCommitId   :: String
+cProjectGitCommitId   = "da53a348150d30193a6f28e1b7ddcabdf45ab726"
+
+cProjectVersion       :: String
+cProjectVersion       = "9.0.1"
+
+cProjectVersionInt    :: String
+cProjectVersionInt    = "900"
+
+cProjectPatchLevel    :: String
+cProjectPatchLevel    = "1"
+
+cProjectPatchLevel1   :: String
+cProjectPatchLevel1   = "1"
+
+cProjectPatchLevel2   :: String
+cProjectPatchLevel2   = ""
