f241e5772fe95810e9b87a597ca1dfd6d7ab28e5
amesgen <amesgen@amesgen.de>
2021-10-10 22:56:01 +0200

    Add example project

---
+++ default.nix
@@ -0,0 +1,12 @@
+let
+  sources = import ./nix/sources.nix { };
+  haskellNix = import sources.haskellNix { };
+  pkgs = import haskellNix.sources.nixpkgs-unstable haskellNix.nixpkgsArgs;
+in
+pkgs.haskell-nix.project {
+  src = pkgs.haskell-nix.haskellLib.cleanGit {
+    name = "stuff";
+    src = ./.;
+  };
+  compiler-nix-name = "ghc8107";
+}
