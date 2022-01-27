c26546fbf7cf60cfd9db9bae9e8a98e227304cc1
amesgen <amesgen@amesgen.de>
2022-01-27 16:01:13 +0100

    Add macdylibbundler overlay

--- default.nix
+++ default.nix
@@ -1,7 +1,24 @@
 let
-  sources = import ./nix/sources.nix { };
-  haskellNix = import sources.haskellNix { };
-  pkgs = import haskellNix.sources.nixpkgs-unstable haskellNix.nixpkgsArgs;
+  pkgs =
+    let
+      sources = import ./nix/sources.nix { };
+      haskellNix = import sources.haskellNix { };
+      inherit (haskellNix) nixpkgsArgs;
+      overlays = nixpkgsArgs.overlays ++ [
+        (self: super: {
+          macdylibbundler = super.macdylibbundler.overrideAttrs (old: {
+            version = "custom";
+            src = super.fetchFromGitHub {
+              owner = "amesgen";
+              repo = "macdylibbundler";
+              rev = "f98d06980e718947b1d4afed3586f475056563b6";
+              sha256 = "0qqgcp14vvr6r5yqj96hy3klc2dkcqyq909az30ql4y879r3xm62";
+            };
+          });
+        })
+      ];
+    in
+    import haskellNix.sources.nixpkgs-unstable haskellNix.nixpkgsArgs;
   hsPkgs = pkgs.haskell-nix.project {
     src = pkgs.haskell-nix.haskellLib.cleanGit {
       name = "stuff";
