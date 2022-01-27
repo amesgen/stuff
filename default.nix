fc3a915f687d8dcdc781ae2d26a2243d317d7690
amesgen <amesgen@amesgen.de>
2022-01-27 16:38:27 +0100

    foo

--- default.nix
+++ default.nix
@@ -1,23 +1,7 @@
 let
+  sources = import ./nix/sources.nix { };
   pkgs =
-    let
-      sources = import ./nix/sources.nix { };
-      haskellNix = import sources.haskellNix { };
-      inherit (haskellNix) nixpkgsArgs;
-      overlays = nixpkgsArgs.overlays ++ [
-        (self: super: {
-          macdylibbundler = super.macdylibbundler.overrideAttrs (old: {
-            version = "custom";
-            src = super.fetchFromGitHub {
-              owner = "amesgen";
-              repo = "macdylibbundler";
-              rev = "f98d06980e718947b1d4afed3586f475056563b6";
-              sha256 = "0qqgcp14vvr6r5yqj96hy3klc2dkcqyq909az30ql4y879r3xm62";
-            };
-          });
-        })
-      ];
-    in
+    let haskellNix = import sources.haskellNix { }; in
     import haskellNix.sources.nixpkgs-unstable haskellNix.nixpkgsArgs;
   hsPkgs = pkgs.haskell-nix.project {
     src = pkgs.haskell-nix.haskellLib.cleanGit {
@@ -26,11 +10,25 @@ let
     };
     compiler-nix-name = "ghc8107";
   };
+  macdylibbundler =
+    (pkgs:
+      if true then
+        pkgs.macdylibbundler.overrideAttrs
+          (old: {
+            version = "custom";
+            src = pkgs.fetchFromGitHub {
+              owner = "amesgen";
+              repo = "macdylibbundler";
+              rev = "f98d06980e718947b1d4afed3586f475056563b6";
+              sha256 = "0qqgcp14vvr6r5yqj96hy3klc2dkcqyq909az30ql4y879r3xm62";
+            };
+          }) else pkgs.macdylibbundler)
+      (import sources.nixpkgs { });
 in
 rec {
   exe = hsPkgs.stuff.components.exes.stuff;
   macOS = pkgs.runCommand "stuff-macOS"
-    { buildInputs = [ pkgs.macdylibbundler ]; } ''
+    { buildInputs = [ macdylibbundler ]; } ''
     mkdir -p $out/bin
     cp ${exe}/bin/stuff $out/bin/stuff
     chmod 755 $out/bin/stuff
