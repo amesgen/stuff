6bf5998e88117e239dccfd9f04e195fc055c8ef1
amesgen <amesgen@amesgen.de>
2022-04-24 02:50:50 +0200

    Add preliminary ghc-lib-parser for GHC 9.4

---
+++ 238f15b285d4e95f0718b9f979245785a483c0c2/ghc-lib/stage0/libraries/ghc-boot/build/GHC/Version.hs
@@ -0,0 +1,21 @@
+module GHC.Version where
+
+import Prelude -- See Note [Why do we import Prelude here?]
+
+cProjectGitCommitId   :: String
+cProjectGitCommitId   = "238f15b285d4e95f0718b9f979245785a483c0c2"
+
+cProjectVersion       :: String
+cProjectVersion       = "9.3.20220413"
+
+cProjectVersionInt    :: String
+cProjectVersionInt    = "903"
+
+cProjectPatchLevel    :: String
+cProjectPatchLevel    = "20220413"
+
+cProjectPatchLevel1   :: String
+cProjectPatchLevel1   = "20220413"
+
+cProjectPatchLevel2   :: String
+cProjectPatchLevel2   = "0"
