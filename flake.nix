b086145778d3756c440489a2bb987af64320ee0e
amesgen <amesgen@amesgen.de>
2022-05-28 14:04:21 +0200

    Repro

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    haskellNix.url = "github:input-output-hk/haskell.nix";
+    nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils, haskellNix }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          inherit (haskellNix) config;
+          overlays = [ haskellNix.overlay ];
+        };
+        hsPkgs = pkgs.haskell-nix.cabalProject {
+          src = ./.;
+          compiler-nix-name = "ghc8107";
+        };
+      in
+      {
+        packages.default =
+          hsPkgs.projectCross.mingwW64.hsPkgs.stuff.components.exes.stuff;
+      });
+}
