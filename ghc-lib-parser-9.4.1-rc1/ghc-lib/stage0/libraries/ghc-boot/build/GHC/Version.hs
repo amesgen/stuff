421b2b468b2cb9c1cbaf6ce29945b81fa16eb26f
amesgen <amesgen@amesgen.de>
2022-07-22 17:26:24 +0200

    Add ghc-lib-parser-9.4.1-rc1

---
+++ ghc-lib-parser-9.4.1-rc1/ghc-lib/stage0/libraries/ghc-boot/build/GHC/Version.hs
@@ -0,0 +1,21 @@
+module GHC.Version where
+
+import Prelude -- See Note [Why do we import Prelude here?]
+
+cProjectGitCommitId   :: String
+cProjectGitCommitId   = "7757465f855f8bb893aad302f38ebab3c0b8f5e4"
+
+cProjectVersion       :: String
+cProjectVersion       = "9.4.0.20220721"
+
+cProjectVersionInt    :: String
+cProjectVersionInt    = "904"
+
+cProjectPatchLevel    :: String
+cProjectPatchLevel    = "020220721"
+
+cProjectPatchLevel1   :: String
+cProjectPatchLevel1   = "0"
+
+cProjectPatchLevel2   :: String
+cProjectPatchLevel2   = "20220721"
