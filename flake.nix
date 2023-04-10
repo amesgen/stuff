efade045bfc2a870cdb8aff7e96a502f04c755da
amesgen <amesgen@amesgen.de>
2023-04-10 16:26:23 +0200

    Repro

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs = {
+    haskellNix.url = "github:input-output-hk/haskell.nix";
+    nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils, haskellNix }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          inherit (haskellNix) config;
+          overlays = [ haskellNix.overlay ];
+        };
+        inherit (pkgs) lib;
+        exeFor = compiler-nix-name:
+          let
+            hsPkgs = pkgs.haskell-nix.cabalProject {
+              src = ./.;
+              inherit compiler-nix-name;
+            };
+          in
+          hsPkgs.projectCross.mingwW64.hsPkgs.stuff.components.exes.stuff;
+      in
+      {
+        packages = lib.genAttrs [ "ghc927" "ghc944" "ghc961" ] exeFor;
+      });
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.iog.io"
+      "https://cache.zw3rk.com"
+    ];
+    extra-trusted-public-keys = [
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+      "loony-tools:pr9m4BkM/5/eSTZlkQyRt57Jz7OMBxNSUiMC4FkcNfk="
+    ];
+  };
+}
