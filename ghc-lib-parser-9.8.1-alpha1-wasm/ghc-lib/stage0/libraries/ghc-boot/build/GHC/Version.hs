aa464d317005073cbe4998badb07e9f1fc9a0f9f
amesgen <amesgen@amesgen.de>
2023-08-01 11:22:24 +0200

    init copied ghc-lib-parser-9.8.1-alpha1-wasm

---
+++ ghc-lib-parser-9.8.1-alpha1-wasm/ghc-lib/stage0/libraries/ghc-boot/build/GHC/Version.hs
@@ -0,0 +1,21 @@
+module GHC.Version where
+
+import Prelude -- See Note [Why do we import Prelude here?]
+
+cProjectGitCommitId   :: String
+cProjectGitCommitId   = "62cb821ef2ef0d47c4cf88a6f2aeb7f75106d361"
+
+cProjectVersion       :: String
+cProjectVersion       = "9.8.0.20230727"
+
+cProjectVersionInt    :: String
+cProjectVersionInt    = "908"
+
+cProjectPatchLevel    :: String
+cProjectPatchLevel    = "020230727"
+
+cProjectPatchLevel1   :: String
+cProjectPatchLevel1   = "0"
+
+cProjectPatchLevel2   :: String
+cProjectPatchLevel2   = "20230727"
