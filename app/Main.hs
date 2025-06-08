449771c92b5ee40c4beeb1ec6fac4e4cc9a48d87
Alexander Esgen <alexander.esgen@iohk.io>
2025-06-08 19:39:13 +0200

    Simplify for GHC bug report

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,4 @@
 module Main where
 
-import qualified Cardano.Ledger.Api.Era as L
-import qualified Cardano.Ledger.Api.Tx as L
-
 main :: IO ()
-main = print tx
-  where
-    tx :: L.Tx L.ConwayEra
-    tx = L.mkBasicTx L.mkBasicTxBody
+main = putStrLn "hello"
